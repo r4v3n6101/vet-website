dea86749136ee3a2df5ccbe0734376678242f323
r4v3n6101 <raven6107@gmail.com>
2021-08-02 08:36:13 +0300

    Fix checker rotation

--- public/scripts/modal.js
+++ public/scripts/modal.js
@@ -1,3 +1,24 @@
+function toggleRotate(e) {
+    let closed = e.attr('state');
+    if (closed === "closed") {
+        e.attr('state', 'opened');
+        $({deg: 180}).animate({deg: 0}, {
+            duration: 400,
+            step: function (now) {
+                e.css({transform: 'rotate(' + now + 'deg)'});
+            }
+        });
+    } else {
+        e.attr('state', 'closed');
+        $({deg: 0}).animate({deg: 180}, {
+            duration: 400,
+            step: function (now) {
+                e.css({transform: 'rotate(' + now + 'deg)'});
+            }
+        });
+    }
+}
+
 $(function () {
     $('#reg_success_modal_close_icon').click(function (event) {
         event.preventDefault();
@@ -6,25 +27,21 @@ $(function () {
 
     $('.modal_reg_header').click(function () {
         $('.modal_registration').slideToggle('slow');
-        $('#slider_icon_reg').css({'transform': 'rotate(0)'})
-        return false;
-    });
-
-    $('.services_modal_header').click(function () {
-        $('.services_list_modal').slideToggle('slow');
-        $('#slider_icon_services').css({'transform': 'rotate(0)'})
-        return false;
+        toggleRotate($('#slider_icon_reg'))
     });
 
     $('#slider_icon_reg').click(function () {
         $('.modal_registration').slideToggle('slow');
-        $('#slider_icon_reg').css({'transform': 'rotate(0)'})
-        return false;
+        toggleRotate($('#slider_icon_reg'))
+    });
+
+    $('.services_modal_header').click(function () {
+        $('.services_list_modal').slideToggle('slow');
+        toggleRotate($('#slider_icon_services'));
     });
 
     $('#slider_icon_services').click(function () {
         $('.services_list_modal').slideToggle('slow');
-        $('#slider_icon_services').css({'transform': 'rotate(0)'})
-        return false;
+        toggleRotate($('#slider_icon_services'))
     });
 })
