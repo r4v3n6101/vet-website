d3c4e8958e5b11ed7e0559d293bb869900fcccfa
r4v3n6101 <raven6107@gmail.com>
2021-08-02 06:04:02 +0300

    Reformat code

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -21,40 +21,40 @@ $(function () {
     $('#reg_success_close_button').click(function (event) {
         event.preventDefault();
         $('.reg_success').slideUp();
-        });
+    });
 
     $('#reg_success_modal_close_icon').click(function (event) {
         event.preventDefault();
         $('.reg_success_modal').slideUp();
-        });
+    });
 
     $(document).ready(function () {
         $('.modal_reg_header').click(function () {
             $('.modal_registration').slideToggle('slow');
-            $('#slider_icon_reg').css({'transform':'rotate(0)'})
+            $('#slider_icon_reg').css({'transform': 'rotate(0)'})
             return false;
-            });
         });
+    });
 
     $(document).ready(function () {
         $('.services_modal_header').click(function () {
             $('.services_list_modal').slideToggle('slow');
-            $('#slider_icon_services').css({'transform':'rotate(0)'})
+            $('#slider_icon_services').css({'transform': 'rotate(0)'})
             return false;
-            });
         });
+    });
 
     $(document).ready(function () {
         $('#slider_icon_reg').click(function () {
             $('.modal_registration').slideToggle('slow');
-                $('#slider_icon_reg').css({'transform': 'rotate(0)'})
+            $('#slider_icon_reg').css({'transform': 'rotate(0)'})
             return false;
         });
     });
     $(document).ready(function () {
         $('#slider_icon_services').click(function () {
             $('.services_list_modal').slideToggle('slow');
-            $('#slider_icon_services').css({'transform':'rotate(0)'})
+            $('#slider_icon_services').css({'transform': 'rotate(0)'})
             return false;
         });
     });
